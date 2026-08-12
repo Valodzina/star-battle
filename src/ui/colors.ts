68c20976b4ac23e166f1128034f2bfe142446e1a
Ulyana Valodzina <ulyanavalodzina@gmail.com>
2026-08-12 15:51:47 +0300

    design

--- src/ui/colors.ts
+++ src/ui/colors.ts
@@ -11,27 +11,33 @@ export const COLORS = {
   text: 0xffffff,
   textMuted: 0xb0b0c8,
   title: 0xe8e8f0,
-  gridLine: 0xe8e8f0,
-  regionBorder: 0xe8e8f0,
-  boardUnderlay: 0xe8e8f0,
+
+  //gridLine: 0xe8e8e8,
+  // regionBorder: 0xe8e8e8,
+  // boardUnderlay: 0xe8e8e8,
+  gridLine: 0x44505c,
+  regionBorder: 0x44505c,
+  boardUnderlay: 0x44505c,
   cellPlayable: 0x2e2e4e,
-  cellEmpty: 0x1a1a2e,
+
   elementFill: 0xf4a261,
-  dotFill: 0xffffff,
+  dotFill: 0x677482,
   victoryOverlay: 0x000000,
   victoryText: 0xffffff,
   answerColor: 0x000000,
 } as const;
 
 export const REGION_COLORS: readonly number[] = [
-  0x3d5a80,
-  0x4a7c59,
-  0x7b5e57,
-  0x6b4c9a,
-  0x8b6914,
-  0x4a6670,
-  0x7a4a6b,
-  0x5a7060,
+  0x9CB8CB,
+  0x8EA5C0,
+  0xA098BE,
+  0xB39BB8,
+  0xC49FAA,
+  0xC39B87,
+  0xC5B187,
+  0xA5B996,
+  0x91ADA0,
+  0xB6A0A9,
 ] as const;
 
 export function getRegionColor(regionId: number): number {
